7b9b21c8849b8e1294c72d7e6129dd8f9c2dabbf
Michael Trotter <spicydonuts@gmail.com>
2013-12-03 13:10:39 -0700

    nuget settings

--- Crypto/Properties/AssemblyInfo.cs
+++ Crypto/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Crypto")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CryptoAPI library for using PEM key format in .Net apps. Based on Christian Etter's CryptoAPI wrapper.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Michael Trotter")]
 [assembly: AssemblyProduct("Crypto")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
